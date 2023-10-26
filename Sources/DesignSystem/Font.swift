f9f5b7b23963e1235c411f9879442d9167106b92
Kristel Maximova <idmaksimovak@gmail.com>
2023-10-26 17:21:19 +0300

    Шрифты сделаны публичными

--- Sources/DesignSystem/Font.swift
+++ Sources/DesignSystem/Font.swift
@@ -14,75 +14,75 @@ extension Font {
         // Header
         public struct Header {
             /// Header 1
-            static let h1 = Font.system(size: 38, weight: .light)
+            public static let h1 = Font.system(size: 38, weight: .light)
             /// Header 2
-            static let h2 = Font.system(size: 24, weight: .medium)
+            public static let h2 = Font.system(size: 24, weight: .medium)
             /// Header 3
-            static let h3 = Font.system(size: 22, weight: .light)
+            public static let h3 = Font.system(size: 22, weight: .light)
             /// Header 4
-            static let h4 = Font.system(size: 18, weight: .medium)
+            public static let h4 = Font.system(size: 18, weight: .medium)
         }
         
         // Body
         public struct Body {
             /// Body Text Medium 16
-            static let b1 = Font.system(size: 16, weight: .medium)
+            public static let b1 = Font.system(size: 16, weight: .medium)
             /// Body Text Regular 16
-            static let b2 = Font.system(size: 16, weight: .regular)
+            public static let b2 = Font.system(size: 16, weight: .regular)
             /// Body Text Medium 14
-            static let b3 = Font.system(size: 14, weight: .medium)
+            public static let b3 = Font.system(size: 14, weight: .medium)
             /// Body Text Regular 14
-            static let b4 = Font.system(size: 14, weight: .regular)
+            public static let b4 = Font.system(size: 14, weight: .regular)
         }
         
         // Comment
         public struct Comment {
             /// Comment Medium
-            static let c1 = Font.system(size: 12, weight: .medium)
+            public static let c1 = Font.system(size: 12, weight: .medium)
             /// Comment regular
-            static let c2 = Font.system(size: 12, weight: .regular)
+            public static let c2 = Font.system(size: 12, weight: .regular)
         }
         
         // Link
         public struct Link {
             /// Link/Text 14
-            static let text14 = Font.system(size: 14, weight: .medium)
+            public static let text14 = Font.system(size: 14, weight: .medium)
         }
         
         // NavigationBar
         public struct NavigationBar {
             /// Navigation bar/Arrow
-            static let arrow = Font.system(size: 16, weight: .medium)
+            public static let arrow = Font.system(size: 16, weight: .medium)
         }
         
         // Button
         public struct Button {
             /// Button
-            static let button = Font.system(size: 16, weight: .medium)
+            public static let button = Font.system(size: 16, weight: .medium)
             /// Small button
-            static let smallButton = Font.system(size: 14, weight: .medium)
+            public static let smallButton = Font.system(size: 14, weight: .medium)
         }
         
         // Native
         public struct Native {
             /// Navigation text
-            static let navigationText = Font.system(size: 17, weight: .regular)
+            public static let navigationText = Font.system(size: 17, weight: .regular)
             /// Native/Title
-            static let title = Font.system(size: 17, weight: .semibold)
+            public static let title = Font.system(size: 17, weight: .semibold)
         }
         
         // Amount
         public struct Amount {
             /// Big amount
-            static let bigAmount = Font.system(size: 24, weight: .medium)
+            public static let bigAmount = Font.system(size: 24, weight: .medium)
             /// Amount bold
-            static let amountBold = Font.system(size: 20, weight: .bold)
+            public static let amountBold = Font.system(size: 20, weight: .bold)
         }
         
         // Timer
         public struct Timer {
             /// Timer/bold
-            static let bold = Font.system(size: 24, weight: .bold)
+            public static let bold = Font.system(size: 24, weight: .bold)
         }
     }
 }
